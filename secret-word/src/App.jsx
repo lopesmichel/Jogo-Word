487afa0961dab3d4f8c0977a15bc190f5a207f0a
Michel Lopes <lopesmichel.lp@gmail.com>
2025-04-06 22:44:05 -0300

    inicio projeto

---
+++ secret-word/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './App.css'
+
+import { useCallback, useEffect, useState } from "react";
+
+// data
+import { wordsList } from "./data/words";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
+
+import StartScreen from "./components/StartScreen";
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <StartScreen />
+    </div>
+  );
+}
+
+export default App
